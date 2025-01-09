3b67b172b391295e48ed6bef4e7edeea539a7827
Nithya S <157941185+Nithya11ramesh@users.noreply.github.com>
2025-01-09 14:08:15 +0530

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
                     <Col md={1} className="text-center mb-md-2">
                         
                         <ul className="list-unstyled">
-                              <li><a href="/Resources" className="text-light text-decoration-none">Resources</a></li>
+                           <li><a href="/Resources" className="text-light text-decoration-none">Resources</a></li>
                             <li><a href="/careers" className="text-light text-decoration-none">Careers</a></li>
                             <li><a href="/blog" className="text-light text-decoration-none">Blog</a></li>
                             
