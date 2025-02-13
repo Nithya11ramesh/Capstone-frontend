26c45e1b7cb8c2d9aa206894f5a2841f663423af
Nithya S <157941185+Nithya11ramesh@users.noreply.github.com>
2025-02-13 12:45:59 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -128,9 +128,9 @@ const Home = () => {
           style={{
             position: 'absolute',
             top: 0,
-            left: 0,
+            left: 50%,
             width: '100%',
-            height: '100%',
+            height: '200%',
             objectFit: 'cover',
             zIndex: 0
           }}
